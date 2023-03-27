aa1e7dc96d359349b069abaf9a061cca4d9a3220
Mohab Alfarra <modo1631@gmail.com>
2023-03-27 12:26:31 +0100

    modified Sign in

--- client/src/components/SignIn/signIn.js
+++ client/src/components/SignIn/signIn.js
@@ -3,9 +3,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Navbar from '../NavBar/NavBar';
 import Box from '@material-ui/core/Box';
-import './signIn.css'
+import './signIn.css';
 import axios from 'axios';
-import image2 from './image2.png'
+import image2 from './image2.png';
 
 function SignIn() {
   const [userName, setUserName] = useState('');
@@ -28,43 +28,48 @@ function SignIn() {
       setError(errorMsg);
     });
   };
-  return (
-    <>
-    <Navbar />
-    <div className="container">
-      <img src={image2} alt="Some image" className="image2" />
-      <Box>
-        <div className="formContainer">
-          <h1>Sign In</h1>
-          <TextField
-            className="formField"
-            label="User Name"
-            value={userName} onChange={(e) => setUserName(e.target.value)}
-            variant="outlined"
-            type="text"
-            fullWidth
-          />
 
-          <TextField
-            className="formField"
-            label="Password"
-            value={password} onChange={(e) => setPassword(e.target.value)}
-            variant="outlined"
-            type="password"
-            fullWidth
-          />
-          {error && <p className="errorMsg">{error}</p>}
-          <Button variant="contained"
-            color="primary"className="submitButton"
-            onClick={handleSignIn} 
-            style={{ backgroundColor: '#F29913', color: '#FFFFFF', width: '100%', margin:"30px 0 0" }}> 
-             Sign In
-          </Button>      
-        </div>
-      </Box>
-    </div>
+  return (
+  <>
+  <Navbar />
+  <div className="container">
+  <img src={image2} alt="Some image" className="image2" />
+  <Box>
+  <div className="formContainer">
+  <h1>Sign In</h1>
+  <div className="textFieldContainer">
+  <TextField
+  className="formField"
+  label="User Name"
+  value={userName} onChange={(e) => setUserName(e.target.value)}
+  variant="outlined"
+  type="text"
+  fullWidth
+  />
+  </div>
+  <div className="textFieldContainer">
+  <TextField
+  className="formField"
+  label="Password"
+  value={password} onChange={(e) => setPassword(e.target.value)}
+  variant="outlined"
+  type="password"
+  fullWidth
+  />
+  </div>
+  {error && <p className="errorMsg">{error}</p>}
+  <Button variant="contained"
+  color="primary"className="submitButton"
+  onClick={handleSignIn}
+  style={{ backgroundColor: '#F29913', color: '#FFFFFF', width: '100%', margin:"30px 0 0" }}>
+  Sign In
+  </Button>
+  </div>
+  </Box>
+  </div>
   </>
-  );
+);
 }
 
 export default SignIn;
+     
